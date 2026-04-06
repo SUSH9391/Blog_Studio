abcad58a5699026295710ccec0a13d6c09adee90
Sushmitha Baliga B <masrbaliga@gmail.com>
2026-04-06 15:10:58 +0530

    fix vercel build

--- vercel_app.py
+++ vercel_app.py
@@ -1,12 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 import traceback
 
+app = FastAPI()
+
 try:
-    from main import app
+    from main import app as real_app
+    app = real_app
 except Exception as e:
-    from fastapi import FastAPI
-    from fastapi.responses import PlainTextResponse
-    
-    app = FastAPI()
     error_msg = traceback.format_exc()
     @app.api_route("/{path_name:path}", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH", "TRACE"])
     async def catch_all(path_name: str):
